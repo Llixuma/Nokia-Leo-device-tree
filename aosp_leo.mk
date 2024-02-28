ee43d965b267c0311670cd00b207ebbab3b924d4
Llixuma <fihlf@outlook.com>
2024-02-28 18:37:06 +0100

    remove build fingerprint

--- aosp_leo.mk
+++ aosp_leo.mk
@@ -9,12 +9,8 @@ PRODUCT_BRAND := nokia
 PRODUCT_MODEL := Nokia 6300 4G
 PRODUCT_MANUFACTURER := Nokia
 
-BUILD_FINGERPRINT := "Nokia/Nokia_6300_4G/Nokia_6300_4G:6.0.1/V1/1.00.00.01:user/release-keys"
-
 $(call inherit-product-if-exists, vendor/nokia/leo/leo-vendor.mk)
 
 PRODUCT_PACKAGES += \
     Launcher3 \
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="Nokia/Nokia_6300_4G/Nokia_6300_4G:6.0.1/V1/1.00.00.01:user/release-keys"
